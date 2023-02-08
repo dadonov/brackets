fbd09f335e1a28d5413212e8a753635d9756c180
dadonov <dadonov.igor@gmail.com>
2023-02-08 18:00:57 +0400

    feat: add solution, cases with the same opening and closing elements are not passing the tests

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let stack = [];
+  const openingElement = [];
+  const elementPairs = {};
+
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    openingElement.push(bracketsConfig[i][0]);
+    elementPairs[bracketsConfig[i][1]] = bracketsConfig[i][0];
+  }
+
+  for (let i = 0; i < str.length; i++) {
+    let currentElement = str[i];
+    if (openingElement.includes(currentElement)) {
+      stack.push(currentElement);
+    }
+    let topStackElement = stack[stack.length - 1];
+    if (elementPairs[currentElement] === topStackElement) {
+      stack.pop();
+    }
+  }
+  return stack.length === 0;
+};
+
